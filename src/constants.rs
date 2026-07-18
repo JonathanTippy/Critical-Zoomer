64bcb876badd1d8fe717de9681cd27373c91dead
Jonathan Tippy <jonathan@tippy.name>
2026-07-18 14:01:38 -0500

    PO: fixed default res

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,8 @@
 use crate::assemblies::structs::*;
 
 // UNDERIVED CONSTANTS
-pub const DEFAULT_WINDOW_RES:(u32, u32) = (800, 400);
+pub const DEFAULT_DEFAULT_WINDOW_RES:(u32, u32) = (854,480);
+pub const DEFAULT_WINDOW_RES:(u32, u32) = DEFAULT_DEFAULT_WINDOW_RES;//(1920, 1080);
 pub const HOME_POSITION:(i32, i32, i32) = (-2, -2, -2);
 pub const MOVE_SPEED_PPS: i32 = 200;
 pub const MOVE_SPEED_IN_SCREENS: f32 = 0.42;
